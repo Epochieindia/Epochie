81e357bee0da39b1f0b59af3479a7223a667964c
muke2110 <muke2110@gmail.com>
2024-10-31 19:48:11 +0530

    Adding Server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const cron = require('node-cron');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve all static files from the root folder
+app.use(express.static(path.join(__dirname)));
+
+// Route to serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Cron job to ping the server every 4 minutes to keep it active
+cron.schedule('*/4 * * * *', () => {
+  console.log('Pinging the server to keep it active...');
+  fetch(`${WEBAPP_URL}checkHealth`,{method: 'GET'})
+      .then(res => res.text())
+      .then(body => console.log(`Server response: ${body}`))
+      .catch(err => console.error('Error pinging the server:', err));
+});
+
+app.get('/checkHealth',(req,res)=>{
+  res.status(200).json("All ok!!!");
+})
