3d70cdf77398a4853606cadbaa5d6d9a92d020bd
muke2110 <muke2110@gmail.com>
2024-10-31 20:01:51 +0530

    Adding Server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cron = require('node-cron');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Serve all static files from the root folder
 app.use(express.static(path.join(__dirname)));
